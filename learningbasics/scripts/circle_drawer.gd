e5b6bc2069d63ac2ce438b7a68081b7e72e5cfbb
Guillermo Gil <guillermo.gil@amigo.org.mx>
2026-05-15 12:33:42 -0600

    Explored and added camera node

--- learningbasics/scripts/circle_drawer.gd
+++ learningbasics/scripts/circle_drawer.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 const RADIUS := 30.0
-const COLOR := Color(0.0, 1.0, 0.0)
+const COLOR := Color(0.81, 0.813, 0.953, 1.0)
 
 func _draw() -> void:
 	draw_circle(Vector2.ZERO, RADIUS, COLOR)
